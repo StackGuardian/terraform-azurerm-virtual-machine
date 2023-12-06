3fc9ac494a32296188b3587de0cda24c456b37b9
Johannes Scheuerer <joscheuerer@gmail.com>
2023-12-06 09:54:41 +0100

    Create nsg.tf

---
+++ nsg.tf
@@ -0,0 +1,21 @@
+resource "azurerm_network_security_group" "nsg" {
+  location            = var.location
+  name                = "nsg-${random_id.id.hex}"
+  resource_group_name = var.resource_group_name
+
+  dynamic "security_rule" {
+    for_each = var.create_public_ip ? ["ssh"] : []
+
+    content {
+      access                     = "Allow"
+      direction                  = "Inbound"
+      name                       = "ssh"
+      priority                   = 200
+      protocol                   = "Tcp"
+      destination_address_prefix = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = local.public_ip
+      source_port_range          = "*"
+    }
+  }
+}
